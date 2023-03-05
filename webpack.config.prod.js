7518c53999f33c542e8b07765ca6b0df063a75a5
RuslanSkolotiy <77191756+RuslanSkolotiy@users.noreply.github.com>
2023-03-05 21:42:00 +0300

    Update webpack.config.prod.js

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-const { merge } = require("webpack-merge");
-const commonConfig = require("./webpack.config.common");
-=======
 const { merge } = require("webpack-merge")
 const commonConfig = require("./webpack.config.common")
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
->>>>>>> 7d62c98ad3bf8d57651aa1bf2b1155f5bb20dd8a
 
 module.exports = merge(commonConfig, {
     mode: "production",
